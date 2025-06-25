d71772beb1e6a5ea0da36833383676099ebf12a0
ARUN PRAKASH E <155139968+arunprakash-2006@users.noreply.github.com>
2025-06-25 11:34:56 +0530

    Time: 93 ms (85.04%), Space: 49.3 MB (58.33%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -10,13 +10,13 @@ class Solution {
         for(int i=2;i*i<=n;i++)
         {
             if(arr[i])
-            {
+            
             for(int j=i*i;j<=n;j+=i)
             {
                 arr[j]=false;
             }
             }
-        }
+        
         for(int i=2;i<n;i++)
         {
         if(arr[i])
