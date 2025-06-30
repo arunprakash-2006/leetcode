13a6f4ed5a8d2ec87dbc3c22794aba770727c495
ARUN PRAKASH E <155139968+arunprakash-2006@users.noreply.github.com>
2025-06-30 22:03:35 +0530

    Time: 2 ms (30.54%), Space: 43.6 MB (44.3%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+        Set<List<Integer>> ans = new HashSet<>(); 
+        List<Integer> a1=new ArrayList<>();
+        Arrays.sort(nums);
+        back(0,a1,ans,nums);
+        return new ArrayList<>(ans);
+    }
+    public void back(int ind,List<Integer> a1, Set<List<Integer>> ans,int[] nums)
+    {
+        if(ind>=nums.length)
+        {
+            ans.add(new ArrayList<>(a1));
+            return;
+        }
+        a1.add(nums[ind]);
+        back(ind+1,a1,ans,nums);
+        a1.remove(a1.size()-1);
+        back(ind+1,a1,ans,nums);
+
+    }
+
+}
