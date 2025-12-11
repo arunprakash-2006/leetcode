5565e78c0038bd7231cbf6a0c5129dadcace2fc0
ARUN PRAKASH E <155139968+arunprakash-2006@users.noreply.github.com>
2025-12-11 11:14:41 +0530

    Time: 0 ms (100%), Space: 41.8 MB (55.94%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0 || n==1) return n; 
+        int tab[]=new int[n+1];
+        tab[0]=0;
+        tab[1]=1;
+        tab[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            tab[i]=tab[i-1]+tab[i-2]+tab[i-3];
+        }
+
+        return tab[n];
+        
+    }
+}
