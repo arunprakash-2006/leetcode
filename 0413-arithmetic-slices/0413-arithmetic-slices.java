ce2dc39c0ad89672766b305418bc6843a20a622b
ARUN PRAKASH E <155139968+arunprakash-2006@users.noreply.github.com>
2026-06-05 21:04:35 +0530

    Time: 0 ms (100%), Space: 42.8 MB (97.76%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int sum=0;
+        int curr=0;
+        if(nums.length<3) return 0;
+        for(int i=2;i<nums.length;i++)
+        {
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
+            {
+                curr++;
+                sum+=curr;
+            }
+            else
+            curr=0;
+        }
+        
+        return sum;
+    }
+}
